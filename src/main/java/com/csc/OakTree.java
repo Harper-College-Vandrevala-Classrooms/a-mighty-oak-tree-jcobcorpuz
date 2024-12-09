c59c8d3ea304f11bf52b6fcf01669132e9eed818
jcobcorpuz <cc62200@mail.harpercollege.edu>
2024-12-08 22:34:12 -0600

    finished lab

--- src/main/java/com/csc/OakTree.java
+++ src/main/java/com/csc/OakTree.java
@@ -1,7 +1,70 @@
 package com.csc;
 
 public class OakTree {
-  public static void main(String[] args) {
-    System.out.println("Oak Tree!!!");
+  public class Squirrel{
+    private String name;
+
+    Squirrel(String name){
+      this.name = name;
+    }
+
+    String getName(){
+      return this.name;
+    }
   }
+
+  public class Node {
+    private Squirrel data;
+    private Node left;
+    private Node right;
+
+    public Node(Squirrel data){
+      this.data = data;
+      this.left = null;
+      this.right = null;
+    }
+
+    public void setLeft(Node left){
+      this.left = left;
+    }
+
+    public void setRight(Node right){
+      this.right = right;
+    }
+
+    public Node getLeft(){
+      return this.left;
+    }
+
+    public Node getRight(){
+      return this.right;
+    }
+
+    public Squirrel getData(){
+      return this.data;
+    }
+  }
+
+  private Node root;
+
+  public OakTree(){
+    this.root = null;
+  }
+
+  public void setRoot(Squirrel data){
+    this.root = new Node(data);
+  }
+
+  public Node getRoot(){
+    return this.root;
+  }
+
+  public void setLeft(Node parent, Squirrel data){
+    parent.setLeft(new Node(data));
+  }
+
+  public void setRight(Node parent, Squirrel data){
+    parent.setRight(new Node(data));
+  }
+
 }
--- src/test/java/com/csc/TestOakTree.java
+++ src/test/java/com/csc/TestOakTree.java
@@ -18,4 +18,51 @@ public class TestOakTree {
   void itWorks() {
     assertEquals(true, true);
   }
+
+  @Test
+  void testAddRoot(){
+    OakTree.Squirrel cheeks = tree.new Squirrel("Cheeks");
+    tree.setRoot(cheeks);
+
+    assertEquals("Cheeks", tree.getRoot().getData().getName());
+  }
+
+  @Test
+  void testAddLeft(){
+    OakTree.Squirrel cheeks = tree.new Squirrel("Cheeks");
+    OakTree.Squirrel squeaks = tree.new Squirrel("Squeaks");
+
+    tree.setRoot(cheeks);
+    OakTree.Node rootNode = tree.getRoot();
+    tree.setLeft(rootNode, squeaks);
+
+    assertEquals("Squeaks", rootNode.getLeft().getData().getName());
+  }
+
+  @Test
+  void testAddRight(){
+    OakTree.Squirrel cheeks = tree.new Squirrel("Cheeks");
+    OakTree.Squirrel fluffybutt = tree.new Squirrel("Mr. Fluffy Butt");
+
+    tree.setRoot(cheeks);
+    OakTree.Node rootNode = tree.getRoot();
+    tree.setRight(rootNode, fluffybutt);
+
+    assertEquals("Mr. Fluffy Butt", rootNode.getRight().getData().getName());
+  }
+
+  @Test
+  void testRetrieve(){
+    OakTree.Squirrel cheeks = tree.new Squirrel("Cheeks");
+    OakTree.Squirrel squeaks = tree.new Squirrel("Squeaks");
+    OakTree.Squirrel fluffybutt = tree.new Squirrel("Mr. Fluffy Butt");
+
+    tree.setRoot(cheeks);
+    OakTree.Node rootNode = tree.getRoot();
+    tree.setLeft(rootNode, squeaks);
+    tree.setRight(rootNode, fluffybutt);
+
+    assertEquals("Squeaks", rootNode.getLeft().getData().getName());
+    assertEquals("Mr. Fluffy Butt", rootNode.getRight().getData().getName());
+  }
 }
